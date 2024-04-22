1f24a39d33749480fd5a948cad7da5a7d3176951
kylaerod <kylarodriguez52@gmail.com>
2024-04-22 13:58:12 -0700

    removed uneccessarry files

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,14 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+// Environment variables was defined in Render.com dashboard
+// MONGODB_URI was defined in Render 
+mongoose.connect(
+ process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // Add other options if needed
+  });
 
 module.exports = mongoose.connection;
--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@ const { gql } = require('apollo-server-express');
 const { ApolloServer } = require('@apollo/server');
 const path = require('path');
 const { User } = require('./models');
-const userRouter = require('./routes/api/users');
 const { expressMiddleware } = require('@apollo/server/express4');
 const db = require('./config/connection');
 const { authMiddleware } = require('./utils/auth');
