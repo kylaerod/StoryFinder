e05e2fbf3425c7996f7abf3061cc2fde476f8acc
kylaerod <kylarodriguez52@gmail.com>
2024-04-22 22:54:41 -0700

    added path

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const { User } = require('./models');
 const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
