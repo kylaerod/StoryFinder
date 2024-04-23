3cfeebe2e72371c4a13151f56b099588bc4e8aef
kylaerod <kylarodriguez52@gmail.com>
2024-04-22 23:51:30 -0700

    .

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
 const authMiddleware = require('./utils/auth');
 const db = require('./config/connection');
-
+const path = require('path');
 
 // Define your schema
 const typeDefs = `
